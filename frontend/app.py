6ca8bb1e4ac249cdf5326d0b430e8d46604b1375
Yan Budakyan <astromid@gmail.com>
2022-07-08 23:34:41 +0300

    Some boilerplate code

--- frontend/app.py
+++ frontend/app.py
@@ -1,2 +1,1 @@
-import httpx
 import streamlit as st
--- make-dev-env.sh
+++ make-dev-env.sh
@@ -5,5 +5,5 @@ mamba env create -f environment-dev.yml
 conda activate egame-179
 conda config --add channels conda-forge
 python -m pip install --no-deps pyecharts streamlit-echarts compress-pickle
-python -m pip install --no-deps extra-streamlit-components stqdm streamlit-autorefresh streamlit-server-state streamlit-option-menu
+python -m pip install --no-deps extra-streamlit-components stqdm streamlit-autorefresh streamlit-server-state
 python -m pip install --no-deps -e backend/ frontend/
