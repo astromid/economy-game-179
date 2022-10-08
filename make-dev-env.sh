071e661142c393e4aef79058af6889bd66902aaf
Yan Budakyan <astromid@gmail.com>
2022-10-08 12:35:17 +0300

    Added server state package to frontend dependencies

--- make-dev-env.sh
+++ make-dev-env.sh
@@ -4,5 +4,6 @@ source ~/miniconda3/etc/profile.d/conda.sh
 mamba env create -f environment-dev.yml
 conda activate egame-179
 conda config --env --add channels conda-forge
-python -m pip install --no-deps pyecharts streamlit-echarts streamlit-option-menu compress-pickle
+python -m pip install --no-deps pyecharts compress-pickle
+python -m pip install --no-deps streamlit-echarts streamlit-option-menu streamlit-server-state
 python -m pip install --no-deps -e backend/ frontend/
