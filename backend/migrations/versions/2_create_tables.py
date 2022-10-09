8615299b16975f913781fec63dd57dde404b386b
Yan Budakyan <astromid@gmail.com>
2022-10-09 13:31:19 +0300

    Adjust second migration, wio

--- backend/migrations/versions/2_create_tables.py
+++ backend/migrations/versions/2_create_tables.py
@@ -7,6 +7,7 @@ Create Date: 2022-10-07 17:32:00.000000
 """
 import sqlalchemy as sa
 from alembic import op
+from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
 revision = "2"
@@ -19,7 +20,7 @@ def upgrade() -> None:
     op.create_table(  # users table (root, players, NPCs)
         "users",
         sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("name", sa.Text),
+        sa.Column("name", sa.Text, nullable=False),
         sa.Column("login", sa.Text),
         sa.Column("password", sa.Text),
     )
@@ -36,8 +37,8 @@ def upgrade() -> None:
     op.create_table(  # markets graph table (as an adjacency list)
         "markets",
         sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("name", sa.Text),
-        sa.Column("ring", sa.Integer),  # 2, 1, 0
+        sa.Column("name", sa.Text, nullable=False),
+        sa.Column("ring", sa.Integer, nullable=False),  # 2, 1, 0
         sa.Column("link1", sa.Integer),
         sa.Column("link2", sa.Integer),
         sa.Column("link3", sa.Integer),
@@ -69,6 +70,7 @@ def upgrade() -> None:
     op.create_table(  # money transactions table
         "transactions",
         sa.Column("id", sa.Integer, autoincrement=True, primary_key=True),
+        sa.Column("timestamp", sa.DateTime, default=mysql.CURRENT_TIMESTAMP),
         sa.Column("cycle", sa.Integer, sa.ForeignKey("cycles.cycle")),
         sa.Column("user_id", sa.Integer, sa.ForeignKey("users.id")),
         sa.Column("amount", sa.Float, nullable=False),
@@ -84,6 +86,7 @@ def upgrade() -> None:
     op.create_table(
         "logistics_operations",
         sa.Column("id", sa.Integer, autoincrement=True, primary_key=True),
+        sa.Column("timestamp", sa.DateTime),
         sa.Column("cycle", sa.Integer, sa.ForeignKey("cycles.cycle")),
         sa.Column("user_id", sa.Integer, sa.ForeignKey("users.id")),
         sa.Column("market_id", sa.Integer, sa.ForeignKey("markets.id")),
@@ -91,7 +94,7 @@ def upgrade() -> None:
         sa.Column("description", sa.Text),
     )
     op.create_table(
-        "manufacturing",
+        "products",
         sa.Column("cycle", sa.Integer, sa.ForeignKey("cycles.cycle")),
         sa.Column("user_id", sa.Integer, sa.ForeignKey("users.id")),
         sa.Column("market_id", sa.Integer, sa.ForeignKey("markets.id")),
@@ -111,4 +114,4 @@ def downgrade() -> None:
     op.drop_table("transactions")
     op.drop_table("balances")
     op.drop_table("logistics_operations")
-    op.drop_table("manufacturing")
+    op.drop_table("products")
