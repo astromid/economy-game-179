2c0b231a259bce3c9e53a615c646340fa3a54cc6
Yan Budakyan <astromid@gmail.com>
2023-05-20 14:55:28 +0300

    fix

--- frontend/egame179_frontend/views/admin/__init__.py
+++ frontend/egame179_frontend/views/admin/__init__.py
@@ -2,5 +2,11 @@
 from egame179_frontend.views.admin.dashboard import RootDashboard
 from egame179_frontend.views.admin.markets import RootMarketsView
 from egame179_frontend.views.admin.stocks import RootStocksView
+from egame179_frontend.views.admin.supplies import RootSuppliesView
 
-__all__ = ["RootDashboard", "RootMarketsView", "RootStocksView"]
+__all__ = [
+    "RootDashboard",
+    "RootMarketsView",
+    "RootStocksView",
+    "RootSuppliesView",
+]
