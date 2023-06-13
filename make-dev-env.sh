8204a93beef47dc80366960126dcc348f9366fca
Yan Budakyan <astromid@gmail.com>
2023-06-13 18:34:23 +0300

    Update to python 3.11

--- make-dev-env.sh
+++ make-dev-env.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 source ~/miniconda3/etc/profile.d/conda.sh
 mamba env create -f environment-dev.yml
-conda activate egame-179
+conda activate egame179
 conda config --env --add channels conda-forge
 python -m pip install --no-deps millify pyecharts streamlit-echarts streamlit-option-menu
 python -m pip install --no-deps -e backend/
